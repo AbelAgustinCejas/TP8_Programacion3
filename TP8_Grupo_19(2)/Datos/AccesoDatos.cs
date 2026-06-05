b6bbc2d03d7ded2e7e7b2051e917dd5e9d4d5131
Tobias Lajud <tobiaslajud123@gmail.com>
2026-06-05 12:02:57 -0300

    TP8 V3
    Agrego clases AccesoDatos (Capa Datos), DaoSucursal (Capa Datos), Sucursal (Capa Entidades) y NegocioSucursal (Capa Negocio)
    Desarrollo AccesoDatos y Sucursal.cs

---
+++ TP8_Grupo_19(2)/Datos/AccesoDatos.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class AccesoDatos
+    {
+        String rutaBDSucursales = @"Data Source=localhost\\sqlexpress; Initial Catalog=BDSucursales;Integrated Security=True";
+
+        public AccesoDatos()
+        {
+
+        }
+
+        private SqlConnection ObtenerConexion()
+        {
+            SqlConnection connection = new SqlConnection(rutaBDSucursales);
+
+            try
+            {
+                connection.Open();
+                return connection;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
+            }
+        }
+
+
+        private SqlDataAdapter ObtenerAdaptador(String consulta, SqlConnection connection)
+        {
+            SqlDataAdapter adapter;
+
+            try
+            {
+                adapter = new SqlDataAdapter(consulta, connection);
+                return adapter;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
+            }
+        }
+
+        public DataTable ObtenerTabla(String tabla, String consulta)
+        {
+            DataSet dataSet = new DataSet();
+
+            SqlConnection connection = ObtenerConexion();
+
+            SqlDataAdapter adapter = ObtenerAdaptador(consulta, connection);
+            adapter.Fill(dataSet, tabla);
+
+            connection.Close();
+
+            return dataSet.Tables[tabla];
+        }
+
+        public int EjecutarProcedimientoAlmacenado(SqlCommand command, String storedProcedure)
+        {
+            int filasAfectadas;
+
+            SqlConnection connection = ObtenerConexion();
+
+            command.Connection = connection;
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = storedProcedure;
+
+            filasAfectadas = command.ExecuteNonQuery();
+
+            connection.Close();
+
+            return filasAfectadas;
+        }
+
+        public Boolean existe(String consulta)
+        {
+            Boolean estado = false;
+
+            SqlConnection connection = ObtenerConexion();
+            SqlCommand command = new SqlCommand(consulta, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                estado = true;
+            }
+
+            return estado;
+        }
+
+        public int ObtenerMaximo(String consulta)
+        {
+            int max = 0;
+
+            SqlConnection connection = ObtenerConexion();
+            SqlCommand command = new SqlCommand(consulta, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            if (reader.Read())
+            {
+                max = Convert.ToInt32(reader[0].ToString());
+            }
+
+            return max;
+        }
+
+    }
+
+}
---
+++ TP8_Grupo_19(2)/Entidades/Sucursal.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sucursal
+    {
+        private int _IDSucursal;
+        private string _NombreSucursal;
+        private string _DescripcionSucursal;
+        private int _HorarioSucursal;
+        private int _ProvinciaSucursal;
+        private string _DireccionSucursal;
+        private string _ImagenSucursal;
+
+        public Sucursal()
+        {
+            
+        }
+
+        public int getIDSucursal()
+        {
+            return _IDSucursal;
+        }
+
+        public void setIDSucursal(int IDSucursal)
+        {
+            _IDSucursal = IDSucursal;
+        }
+
+        public string getNombreSucursal()
+        {
+            return _NombreSucursal;
+        }
+
+        public void setNombreSucursal(string NombreSucursal)
+        {
+            _NombreSucursal = NombreSucursal;
+        }
+
+        public string getDescripcionSucursal()
+        {
+            return _DescripcionSucursal;
+        }
+
+        public void setDescripcionSucursal(string DescripcionSucursal)
+        {
+            _DescripcionSucursal = DescripcionSucursal;
+        }
+
+        public int getHorarioSucursal()
+        {
+            return _HorarioSucursal;
+        }
+
+        public void setHorarioSucursal(int HorarioSucursal)
+        {
+            _HorarioSucursal = HorarioSucursal;
+        }
+
+        public int getProvinciaSucursal()
+        {
+            return _ProvinciaSucursal;
+        }
+
+        public void setProvinciaSucursal(int ProvinciaSucursal)
+        {
+            _ProvinciaSucursal = ProvinciaSucursal;
+        }
+
+        public string getDireccionSucursal()
+        {
+            return _DireccionSucursal;
+        }
+
+        public void setDireccionSucursal(string DireccionSucursal)
+        {
+            _DireccionSucursal = DireccionSucursal;
+        }
+
+        public string getImagenSucursal()
+        {
+            return _ImagenSucursal;
+        }
+
+        public void setImagenSucursal(string ImagenSucursal)
+        {
+            _ImagenSucursal = ImagenSucursal;
+        }
+    }
+}
